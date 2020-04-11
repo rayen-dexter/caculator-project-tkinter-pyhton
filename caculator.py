3f9fc826d20c7ea8e6a54491c9ccbff27acef929
rayen-sayadi <63371391+rayen-dexter@users.noreply.github.com>
2020-04-11 16:32:07 +0100

    Add files via upload

--- caculator.py
+++ caculator.py
@@ -2,8 +2,10 @@ from tkinter import *
 win=Tk()
 win.configure(bg='Gold')
 win.title('calculator')
-e=Text(bg='black',fg='Gold',width=20,height=1)
+e=Text(bg='black',fg='Gold',width=20,height=1,font=("Arial Bold", 15))
 e.grid(row=1,column=2)
+def d0 ():
+    e.insert(END,'0')
 def d1 ():
     e.insert(END,'1')
 def d2 ():
@@ -21,25 +23,29 @@ def d7 ():
 def d8 ():
     e.insert(END,'8')
 def d9 ():
-    e.insert(END,'9')    
-b1=Button(text='1',relief=RAISED,width=20,command=d1,bg='Gold')
+    e.insert(END,'9')
+b0=Button(text='0',relief=RAISED,width=20,command=d0,bg='Gold',font=("Arial Bold", 15))
+b0.grid(row=1,column=1)
+b1=Button(text='1',relief=RAISED,width=20,command=d1,bg='Gold',font=("Arial Bold", 15))
 b1.grid(row=2,column=1)
-b2=Button(text='2',relief=RAISED,width=20,command=d2,bg='Gold')
+b2=Button(text='2',relief=RAISED,width=20,command=d2,bg='Gold',font=("Arial Bold", 15))
 b2.grid(row=2,column=2)
-b3=Button(text='3',relief=RAISED,width=20,command=d3,bg='Gold')
+b3=Button(text='3',relief=RAISED,width=20,command=d3,bg='Gold',font=("Arial Bold", 15))
 b3.grid(row=2,column=3)
-b4=Button(text='4',relief=RAISED,width=20,command=d4,bg='Gold')
+b4=Button(text='4',relief=RAISED,width=20,command=d4,bg='Gold',font=("Arial Bold", 15))
 b4.grid(row=3,column=1)
-b5=Button(text='5',relief=RAISED,width=20,command=d5,bg='Gold')
+b5=Button(text='5',relief=RAISED,width=20,command=d5,bg='Gold',font=("Arial Bold", 15))
 b5.grid(row=3,column=2)
-b6=Button(text='6',relief=RAISED,width=20,command=d6,bg='Gold')
+b6=Button(text='6',relief=RAISED,width=20,command=d6,bg='Gold',font=("Arial Bold", 15))
 b6.grid(row=3,column=3)
-b7=Button(text='7',relief=RAISED,width=20,command=d7,bg='Gold')
+b7=Button(text='7',relief=RAISED,width=20,command=d7,bg='Gold',font=("Arial Bold", 15))
 b7.grid(row=4,column=1)
-b8=Button(text='8',relief=RAISED,width=20,command=d8,bg='Gold')
+b8=Button(text='8',relief=RAISED,width=20,command=d8,bg='Gold',font=("Arial Bold", 15))
 b8.grid(row=4,column=2)
-b9=Button(text='9',relief=RAISED,width=20,command=d9,bg='Gold')
+b9=Button(text='9',relief=RAISED,width=20,command=d9,bg='Gold',font=("Arial Bold", 15))
 b9.grid(row=4,column=3)
+global writen
+writen=False
 def add() :
     e.insert(END,'+')
 def  equal() :
@@ -51,19 +57,21 @@ def reset () :
 def substract() :
     e.insert(END,'-')
 def multiply() :
-    e.insert(END,'*')    
+    e.insert(END,'*')
 def divide() :
-    e.insert(END,'/')    
+    e.insert(END,'/')
     
     
+    
 
 
-add_btn=Button(text=('+'),command=add,width=20,bg='Gold').grid(row=5,column=1)
-eql_btn=Button(text=('='),width=20,command=equal,bg='Gold').grid(row=5,column=3)
-reset_btn=Button(text=('reset'),width=20,command=reset,bg='Gold').grid(row=5,column=2)
-sub_btn=Button(text=('-'),width=20,command=substract,bg='Gold').grid(row=6,column=1)
-mul_btn=Button(text=('x'),width=20,command=multiply,bg='Gold').grid(row=6,column=2)
-div_btn=Button(text=('/'),width=20,command=divide,bg='Gold').grid(row=6,column=3)
+add_btn=Button(text=('+'),command=add,width=20,bg='Gold',font=("Arial Bold", 15)).grid(row=5,column=1)
+eql_btn=Button(text=('='),width=20,command=equal,bg='Gold',font=("Arial Bold", 15)).grid(row=5,column=3)
+reset_btn=Button(text=('reset'),width=20,command=reset,bg='Gold',font=("Arial Bold", 15)).grid(row=5,column=2)
+sub_btn=Button(text=('-'),width=20,command=substract,bg='Gold',font=("Arial Bold", 15)).grid(row=6,column=1)
+mul_btn=Button(text=('x'),width=20,command=multiply,bg='Gold',font=("Arial Bold", 15)).grid(row=6,column=2)
+div_btn=Button(text=('/'),width=20,command=divide,bg='Gold',font=("Arial Bold", 15)).grid(row=6,column=3)
 
 
 
+win.mainloop()
